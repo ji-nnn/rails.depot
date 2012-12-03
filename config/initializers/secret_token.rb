dc558bd5be2ef346d88cac4ced5d8a70307376cf
Keishi Mihara <dj.missy.1204+github@gmail.com>
2012-12-04 00:18:06 +0900

    new depot.
    買い物システムでRailsの勉強

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'df9b45ad984840f0b37cead99d8054f2c098ef7046d296fc65d43e8b89df847cefcfe247cc175210935aba1c8924ed015e079dd9862a38e2d132ff8ab19079bb'
