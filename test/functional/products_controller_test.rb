c9f642612b6c266a7e7f5c58da6d43c30d059dc6
Keishi Mihara <dj.missy.1204+github@gmail.com>
2012-12-09 19:34:05 +0900

    implement catalog page(as placeholder)

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,12 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_description' do |elements|
+      elements.each do |element|
+        assert_select element, 'dt', 1
+        assert_select element, 'dd', 1
+      end
+    end
   end
 
   test "should get new" do
@@ -52,4 +58,5 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+
 end
