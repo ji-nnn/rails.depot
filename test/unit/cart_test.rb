bfbbba6490b7d3afaa9ace4ee660133fffd1b1d8
Keishi Mihara <dj.missy.1204+github@gmail.com>
2012-12-19 00:04:07 +0900

    task E, Activity 2 finished.

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -1,18 +1,27 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-	fixtures :carts
-  fixtures :products
   # test "the truth" do
   #   assert true
   # end
 
-  test 'cart should create a new line item when adding a new product' do
-  	cart = Carts.new
-    line_item1 = cart.add_product(products(:ruby).id)
-    line_item2 = cart.add_product(products(:ruby).id)
+  test "add unique products" do
+     cart = Cart.create
+     book_one = products(:ruby)
+     book_two  = products(:rails)
+     cart.add_product(book_one.id).save!
+     cart.add_product(book_two.id).save!
+     assert_equal 2, cart.line_items.size
+     assert_equal book_one.price + book_two.price, cart.total_price
+   end
 
-    assert_same line_item1, line_item2
-  end
-
+   test "add_duplicate_product" do
+     cart = Cart.create
+     ruby_book = products(:rails)
+     cart.add_product(ruby_book.id).save!
+     cart.add_product(ruby_book.id).save!
+     assert_equal 2 * ruby_book.price, cart.total_price
+     assert_equal 1, cart.line_items.size
+     assert_equal 2, cart.line_items[0].quantity
+   end
 end
